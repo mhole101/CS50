21c4b099467e4fad126c7400f07b21b8eaa1358b
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-14 22:37:44 +0000

    Wed, Dec 14, 2022, 10:37 PM +00:00

---
+++ OPP.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using std::string;
+
+class Employee{
+    private:
+    string Name;
+    string Company;
+    int Age;
+
+    public:
+    // Name setter
+    void setName(string name) {
+        Name = name;
+    }
+    // Name getter
+    string getName(){
+        return Name;
+    }
+    // Company setter
+    void setCompany(string company){
+        Company = company;
+    }
+    // Company getter
+    string getCompany(){
+        return Company;
+    }
+    // Age setter
+    void setAge(int age){
+        if(age>18)
+        Age = age;
+    }
+    // Age getter
+    int getAge(){
+        return Age;
+    }
+    void IntroduceYourself()
+
+
+}
+
+void main(void)
+{
+    Employee employee1;
+    Employee employee2;
+
+}
