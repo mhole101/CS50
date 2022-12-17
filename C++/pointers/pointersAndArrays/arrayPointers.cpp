a5c6bad0deb70a043178b6f9c07a3c0e3bf4df84
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 03:06:30 +0000

    Sat, Dec 17, 2022, 3:06 AM +00:00

---
+++ C++/pointers/pointersAndArrays/arrayPointers.cpp
@@ -0,0 +1,16 @@
+// pointers and arrays
+// content taken from "How to use pointers and arrays"
+// https://www.youtube.com/watch?v=DsS-ZiYOWy4&list=PL43pGnjiVwgSSRlwfahAuIqoJ8TfDIlHq&index=3
+//
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int luckNumbers[5] = {2,3,5,7,9};
+    cout << luckNumbers << endl;
+    cout << &luckyNumbers[0] << endl;
+
+    return 0;
+}
