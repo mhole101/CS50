d6068d8e9dca589825f71b2e05259deb1811e911
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 02:33:30 +0000

    Sat, Dec 17, 2022, 2:33 AM +00:00

--- C++/pointers/pointers.cpp
+++ C++/pointers/pointers.cpp
@@ -8,8 +8,13 @@ int main()
 {
     int n = 5;
     cout << &n << endl; //& gives location of memory
-    int* ptr =&n;
+    int* ptr =&n; // sets location of n to ptr
+    cout << ptr << endl; // prints location of n
+    cout << *ptr << endl; // * dereferences pointer
+    *ptr=10;
     cout << ptr << endl;
+    cout << n << endl;
+
 
     //system("pause>0");
     return 0;
