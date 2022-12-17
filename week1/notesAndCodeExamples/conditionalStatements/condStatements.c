ac038551e65bfd1c50308bc2a377e285e6d38779
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 17:43:46 +0000

    Sat, Dec 17, 2022, 5:43 PM +00:00

--- week1/notesAndCodeExamples/conditionalStatements/condStatements.c
+++ week1/notesAndCodeExamples/conditionalStatements/condStatements.c
@@ -147,9 +147,6 @@ int main(void)
         case 1:
             printf("One!\n");
             break;
-        case 1:
-            printf("One!\n");
-            break;
         case 2:
             printf("Two!\n");
             break;
