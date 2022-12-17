c3021822c6e9bd5815752f5b3200ba0df0cb076f
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 03:29:58 +0000

    Sat, Dec 17, 2022, 3:29 AM +00:00

---
+++ C++/pointers/pointersAndArrays/returnMultiple/multipleValues.cpp
@@ -0,0 +1,20 @@
+//
+//
+//
+//
+#include <iostream>
+using namespace std;
+
+int getMin(int numbers[], int size){
+    int min = numbers[];
+    for(int i = 1; i < size; i++)
+        if(numbers[i] < min)
+            min = numbers[i];
+}
+
+int main()
+{
+    int numbers[5]={5,4,-2,29,6};
+
+    return 0;
+}
