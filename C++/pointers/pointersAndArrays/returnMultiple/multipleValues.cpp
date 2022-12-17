9a1c75b2b268953a81ce03052f355b09e1a15236
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 03:38:15 +0000

    Sat, Dec 17, 2022, 3:38 AM +00:00

--- C++/pointers/pointersAndArrays/returnMultiple/multipleValues.cpp
+++ C++/pointers/pointersAndArrays/returnMultiple/multipleValues.cpp
@@ -23,6 +23,14 @@ int getMax(int numbers[], int size){
     return max;
 }
 
+void getMinAndMax(int numbers[], int size, int* min, int* max){
+    for(int i = 1; i < size; i++){
+        if(numbers[i] > *max)
+            *max = numbers[i];
+        if(numbers[i] < *min)
+            *min = numbers[i];
+}
+
 int main()
 {
     int numbers[5]={5,4,-2,29,6};
