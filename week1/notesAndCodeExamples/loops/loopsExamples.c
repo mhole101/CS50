4f4a1ca1c862eedf60c3130e2d523faf62f84a95
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 18:10:51 +0000

    Sat, Dec 17, 2022, 6:10 PM +00:00

---
+++ week1/notesAndCodeExamples/loops/loopsExamples.c
@@ -0,0 +1,22 @@
+// Content taken from week 1 loops
+//
+//  while - use when you want a loop to repeat an
+//          unknown number of times,
+//          and possibly not at all
+//
+//  do-while - use when you wanta loop to repeat an
+//             unknown number of times,
+//             but at least once.
+//
+//  for -   use when you want a loop to repeat a
+//          discrete number of times, though you
+//          may not know the number at the moment
+//          the program is compiled.
+//
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+
+}
