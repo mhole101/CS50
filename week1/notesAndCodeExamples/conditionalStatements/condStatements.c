9e7402c534555ba064bb9c9cbad2d6704857f2a9
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 16:45:11 +0000

    Sat, Dec 17, 2022, 4:45 PM +00:00

---
+++ week1/notesAndCodeExamples/conditionalStatements/condStatements.c
@@ -0,0 +1,11 @@
+// These notes were created with the content from
+// week 1 videos data types, operators, conditional statements
+//
+
+#include <stdio>
+#include <cs50.h>
+
+int main(void)
+{
+    p
+}
