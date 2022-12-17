a64548cf38ba1842f8342161227eeeaf26826d11
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 16:54:47 +0000

    Sat, Dec 17, 2022, 4:54 PM +00:00

--- week1/notesAndCodeExamples/conditionalStatements/condStatements.c
+++ week1/notesAndCodeExamples/conditionalStatements/condStatements.c
@@ -1,9 +1,25 @@
 // These notes were created with the content from
 // week 1 videos data types, operators, conditional statements
 //
+// Data Types
+//      int - 32 bits/4 bytes
+//       range: -2^31 - (2^31)-1
+//
+//      unsigned int - unsigned is a quantifier that can be
+//                      can be used to certain types of data
+//                      which effectively doubles the positive
+//                      range of variables of that type, at the
+//                      cost of disallowing any negative values
+//                      Range 0 to (2^32)-1
+//
+//      char - 8bits,1 byte; used to store single character
+//              Range -128(-2^8) to 127((2^8)-1
 
-#include <stdio>
+
+#include <stdio.h>
 #include <cs50.h>
+
+
 
 int main(void)
 {
