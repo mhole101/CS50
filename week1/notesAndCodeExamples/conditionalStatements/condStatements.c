c04ba36b0b3c7f2de5e60e9cdfc9df737aab47fd
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-17 17:02:08 +0000

    Sat, Dec 17, 2022, 5:02 PM +00:00

--- week1/notesAndCodeExamples/conditionalStatements/condStatements.c
+++ week1/notesAndCodeExamples/conditionalStatements/condStatements.c
@@ -13,9 +13,19 @@
 //                      Range 0 to (2^32)-1
 //
 //      char - 8bits,1 byte; used to store single character
-//              Range -128(-2^8) to 127((2^8)-1
-
-
+//              Range -128(-2^7) to 127((2^7)-1)
+//
+//      float - 32bits/4bytes stores real nubmers (decimal)
+//
+//      double - 64bits/8bytes
+//                 double is used for variable that will store
+//                 store floating-point values.
+//                  doubles have double precision
+//
+//      bool - 1bit?; true or fause; must use #include <cs50.h>
+//
+//      string - store series of characters; must use #include <cs50.h>
+//
 #include <stdio.h>
 #include <cs50.h>
 
