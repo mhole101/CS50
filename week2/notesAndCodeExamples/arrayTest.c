f665c46a340e1cc25b19389aa3eb683ffe58c7aa
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-18 21:28:20 +0000

    Sun, Dec 18, 2022, 9:28 PM +00:00

--- week2/notesAndCodeExamples/arrayTest.c
+++ week2/notesAndCodeExamples/arrayTest.c
@@ -1,8 +1,33 @@
 #include <cs50.h>
 #include <stdio.h>
 
+long getCreditCardNum(void);
+int getCreditCardNumLen(long creditCardNum);
 
 int main (void)
 {
+    long creditCardNum = getCreditCardNum();
+    int creditCardLen = getCreditCardNumLen(creditCardNum);
+    printf("creditCardNum: %li\n", creditCardNum);
+    printf("creditCardLen: %i\n", creditCardLen);
+    long creditCardArray[creditCardLen];
 
+}
+
+long getCreditCardNum(void)
+{
+    long creditCardNum = get_long("Number: ");
+    return creditCardNum;
+}
+
+int getCreditCardNumLen(long creditCardNum)
+{
+    int creditCardLen = 0;
+     while(creditCardNum)
+    {
+        creditCardNum /= 10;
+        creditCardLen++;
+    }
+
+    return creditCardLen;
 }
