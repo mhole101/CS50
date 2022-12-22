ad54c388f587981da2583c37d201e6721311592b
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-22 21:06:57 +0000

    Thu, Dec 22, 2022, 9:06 PM +00:00

--- week2/notesAndCodeExamples/classExamples/argv.c
+++ week2/notesAndCodeExamples/classExamples/argv.c
@@ -4,5 +4,11 @@
 int main(int argc, string argv[])
 {
     printf("argc: %i\n", argc);
-    //printf("%s\n", argv[]);
+    int i = 0;
+    while(i < argc)
+    {
+        printf("%s\n", argv[i]);
+        i++;
+    }
+
 }
