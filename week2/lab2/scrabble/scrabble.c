5b5785a3ea36802dee41b6fc00ac55ea54a58cfb
mhole101 <71180205+mhole101@users.noreply.github.com>
2022-12-26 16:37:22 +0000

    Mon, Dec 26, 2022, 4:37 PM +00:00

--- week2/lab2/scrabble/scrabble.c
+++ week2/lab2/scrabble/scrabble.c
@@ -18,7 +18,7 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Print the winner
     if(score1 > score2)
     {
         printf("Player 1 wins!\n");
@@ -42,25 +42,30 @@ int main(void)
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    // Compute and return score for string
     int score = 0;
 
+    // index through string
     for (int i = 0; i < strlen(word); i++)
     {
         // adds 0 to score if char is not alpha
         if (isalpha(word[i]))
         {
-            //Converts char to uppercase
+            // Converts char to uppercase
             char upperChar = toupper(word[i]);
+
+            // Gets POINTS array inex value from char
             int pointsIndexNum = upperChar - 65;
-            printf("word[%i]: %c is alpha! upper: %c = %i\n", i, word[i], upperChar, pointsIndexNum);
+
+            // Adds char value to score
             score += POINTS[upperChar - 65];
         }
         else
         {
+            // Adds 0 to score
             score += 0;
         }
     }
-    printf("score = %i\n", score);
+
     return score;
 }
